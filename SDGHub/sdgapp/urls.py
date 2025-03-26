2f79022fedd856dab1cca76cf269a78b20a67682
Gobi05-exe <gobicodes@gmail.com>
2025-03-26 21:48:14 +0530

    "added urls and views for sdgapp"

--- SDGHub/sdgapp/urls.py
+++ SDGHub/sdgapp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name = 'sdgapp'
 urlpatterns = [
-    path('home/', views.Home, name='home'),
+    path('dashboard/', views.Dashboard, name='dashboard'),
+    path('collab/',views.Collab,name='collab'),
+    path('funds/',views.Funds,name='funds'),
+    path('settings/',views.Settings,name='settings'),
 ]
 
--- SDGHub/sdgapp/views.py
+++ SDGHub/sdgapp/views.py
@@ -3,6 +3,17 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def Home(request):
-    return render(request, 'index.html')
+def Dashboard(request):
+    return render(request, 'dashboard2.html')
 
+@login_required
+def Collab(request):
+    return render(request, 'collab.html')
+
+@login_required
+def Funds(request):
+    return render(request, 'funds.html')
+
+@login_required
+def Settings(request):
+    return render(request, 'settings.html')
